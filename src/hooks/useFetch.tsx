da5127f291aea3b62112afcd33ebe5a67bfa1f51
jae <albertforus@gmail.com>
2025-01-10 12:04:12 +0800

    feat: add useFetch hook

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+
+interface FetchState<T> {
+  data: null | T
+  loading: boolean
+  error: Error | null
+}
+
+export const useFetch = <T,>(url: string): FetchState<T> => {
+  const [state, setState] = useState<FetchState<T>>({
+    data: null,
+    loading: true,
+    error: null,
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const data = await response.json()
+        console.log(data)
+        setState({ data, loading: false, error: null })
+      } catch (error) {
+        setState({ data: null, loading: false, error: error as Error })
+      }
+    }
+
+    fetchData()
+  }, [url])
+
+  return state
+}
