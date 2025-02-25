62ed4405cd03f3c9d62a7c47600c604d8967e9ea
jl <jaeli_ottawa@outlook.com>
2025-02-25 12:35:42 -0500

    add authContext

--- resources.md
+++ resources.md
@@ -1,2 +1,6 @@
+# Form
 * form: https://github.com/ByteGrad/react-hook-form-with-zod-and-server-side/blob/main/components/form-with-react-hook-form.tsx
-* 
+* from https://github.com/waiting-for-dev/devise-jwt?tab=readme-ov-file#supported-authentication-strategies: "API-only applications don't support browser-based authentication via cookies"
+# auth
+* auth with context: https://github.com/iamshaunjp/MERN-Auth-Tutorial/tree/lesson-17
+* store token in context: https://www.robinwieruch.de/react-router-authentication/
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,54 @@
+import { useFetch } from "@/hooks"
+import { createContext, useEffect, useReducer, Dispatch, ReactNode } from 'react';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface AuthState {
+  user: User | null
+}
+
+type AuthAction = 
+| { type: 'LOGIN', payload: User } 
+| { type: 'LOGOUT' };
+
+interface AuthContextProps { user: User | null, dispatch: Dispatch<AuthAction>}
+
+interface AuthContextProviderProps { children: ReactNode}
+
+export const AuthContext = createContext<AuthContextProps | null>(null)
+
+// use reducer function to manage the state of the user
+export const authReducer = (state: AuthState, action: AuthAction) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return { user: action.payload }
+    case 'LOGOUT':
+      return { user: null }
+    default:
+      return state
+  }
+}
+
+export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+  const [state, dispatch] = useReducer(authReducer, {user: null})
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user')!)
+    if (user){
+      dispatch({ type: 'LOGIN', payload: user })
+    }
+  }, [])
+
+  console.log('AuthContext state:', state)
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
