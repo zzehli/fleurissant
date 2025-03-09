a5b9ab9b461c526d59556f4a32685eb4d89fd838
jl <jaeli_ottawa@outlook.com>
2025-03-09 08:15:08 -0400

    fix: dynamic column numbers with gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -8,11 +8,11 @@ interface GalleryProps {
 
 const Gallery = ({products}:GalleryProps) => {
   const location = useLocation()
-
+  const numCols = location.pathname === '/collection' ? 'lg:grid-cols-3': 'lg:grid-cols-4'
   return (
     <section className="w-full py-2 md:py-2 lg:py-2">
       <div className="container">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:{{location.pathname !== '/' ? grid-cols-4: grid-cols-3}} gap-6 mt-8">
+        <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ${numCols} gap-6 mt-8`}>
           {products?.map((product) => (
             <Card key={product.id} className="overflow-hidden">
               <div className="relative">
