7b83a0190b4ed137687917711d59d6c7596f165b
jl <jaeli_ottawa@outlook.com>
2025-03-12 15:54:16 -0400

    gallery ui change

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,8 +1,6 @@
 import { Card, CardContent, CardDescription, CardFooter, CardTitle } from '@/components/ui/card'
 import { Product } from '@/@types'
-import { useLocation } from 'react-router'
-import { useNavigate } from 'react-router'
-import { NavLink } from "react-router";
+import { useLocation, NavLink } from 'react-router'
 interface GalleryProps {
   products: Product[] | null
 }
@@ -10,7 +8,6 @@ interface GalleryProps {
 const Gallery = ({ products }: GalleryProps) => {
   const location = useLocation()
   const numCols = location.pathname === '/collection' ? 'lg:grid-cols-3' : 'lg:grid-cols-4'
-  const navigate = useNavigate()
   return (
     <section className="w-full py-2 md:py-2 lg:py-2">
       <div className="container">
@@ -21,39 +18,25 @@ const Gallery = ({ products }: GalleryProps) => {
                 key={product.id}
                 className="overflow-hidden"
                 >
-                {/* <div className="relative"> */}
                 <CardContent>
                 <img
                     src={product.image || "https://loremflickr.com/320/240"}
                     alt={product.name}
                     className="object-cover aspect-video rounded-xl w-full h-full"
-                    // style={{ objectFit: 'fill' }}
                   />
                 </CardContent>
                   
-                {/* </div> */}
                 <CardFooter>
                   <p className='text-lg font-semibold'>
                     {product.name}
                   </p>
                   <p>${product.price}</p>
                 </CardFooter>
-                {/* <CardContent>
-                <p className="text-lg font-semibold">{product.price}</p>
-              </CardContent> */}
-                {/* <CardFooter>
-                <Button className="w-full">Add to Cart</Button>
-              </CardFooter> */}
               </Card>
             </NavLink>
 
           ))}
         </div>
-        {/* <div className="flex justify-center mt-12">
-          <Button variant="outline" size="lg">
-            View All Products
-          </Button>
-        </div> */}
       </div>
     </section>
   )
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg text-card-foreground",
       className
     )}
     {...props}
@@ -60,7 +60,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("px-6 pb-2 pt-0", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
@@ -70,7 +70,7 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
+    className={cn("flex flex-col space-y-1 px-6", className)}
     {...props}
   />
 ))
