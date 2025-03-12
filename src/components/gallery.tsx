8f4d37968cecf9b17041d5e003459a0fff5ee648
jl <jaeli_ottawa@outlook.com>
2025-03-12 15:51:21 -0400

    use link on cards

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,43 +1,52 @@
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardFooter, CardTitle } from '@/components/ui/card'
 import { Product } from '@/@types'
 import { useLocation } from 'react-router'
 import { useNavigate } from 'react-router'
+import { NavLink } from "react-router";
 interface GalleryProps {
   products: Product[] | null
 }
 
-const Gallery = ({products}:GalleryProps) => {
+const Gallery = ({ products }: GalleryProps) => {
   const location = useLocation()
-  const numCols = location.pathname === '/collection' ? 'lg:grid-cols-3': 'lg:grid-cols-4'
+  const numCols = location.pathname === '/collection' ? 'lg:grid-cols-3' : 'lg:grid-cols-4'
   const navigate = useNavigate()
   return (
     <section className="w-full py-2 md:py-2 lg:py-2">
       <div className="container">
         <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ${numCols} gap-6 mt-1`}>
           {products?.map((product) => (
-            <Card 
-            key={product.id} 
-            className="overflow-hidden"
-            onClick={()=>navigate(`/collection/${product.id}`)}>
-              <div className="relative">
+            <NavLink to={`/collection/${product.id}`} end>
+              <Card
+                key={product.id}
+                className="overflow-hidden"
+                >
+                {/* <div className="relative"> */}
+                <CardContent>
                 <img
-                  src={product.image || "https://loremflickr.com/320/240"}
-                  alt={product.name}
-                  className="object-cover mx-auto mt-2"
-                  style={{ objectFit: 'fill'}}
-                />
-              </div>
-              <CardHeader>
-                <CardTitle>{product.name}</CardTitle>
-                <CardDescription>{product.description}</CardDescription>
-              </CardHeader>
-              {/* <CardContent>
+                    src={product.image || "https://loremflickr.com/320/240"}
+                    alt={product.name}
+                    className="object-cover aspect-video rounded-xl w-full h-full"
+                    // style={{ objectFit: 'fill' }}
+                  />
+                </CardContent>
+                  
+                {/* </div> */}
+                <CardFooter>
+                  <p className='text-lg font-semibold'>
+                    {product.name}
+                  </p>
+                  <p>${product.price}</p>
+                </CardFooter>
+                {/* <CardContent>
                 <p className="text-lg font-semibold">{product.price}</p>
               </CardContent> */}
-              {/* <CardFooter>
+                {/* <CardFooter>
                 <Button className="w-full">Add to Cart</Button>
               </CardFooter> */}
-            </Card>
+              </Card>
+            </NavLink>
+
           ))}
         </div>
         {/* <div className="flex justify-center mt-12">
