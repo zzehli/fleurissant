33ab1da42f90995e116bb53a5dc58d9eb8062988
jl <jaeli_ottawa@outlook.com>
2025-03-12 12:33:24 -0400

    add onClick to items

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,7 +1,7 @@
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Product } from '@/@types'
 import { useLocation } from 'react-router'
-
+import { useNavigate } from 'react-router'
 interface GalleryProps {
   products: Product[] | null
 }
@@ -9,12 +9,16 @@ interface GalleryProps {
 const Gallery = ({products}:GalleryProps) => {
   const location = useLocation()
   const numCols = location.pathname === '/collection' ? 'lg:grid-cols-3': 'lg:grid-cols-4'
+  const navigate = useNavigate()
   return (
     <section className="w-full py-2 md:py-2 lg:py-2">
       <div className="container">
         <div className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 ${numCols} gap-6 mt-1`}>
           {products?.map((product) => (
-            <Card key={product.id} className="overflow-hidden">
+            <Card 
+            key={product.id} 
+            className="overflow-hidden"
+            onClick={()=>navigate(`/collection/${product.id}`)}>
               <div className="relative">
                 <img
                   src={product.image || "https://loremflickr.com/320/240"}
