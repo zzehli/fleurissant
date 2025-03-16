9e2be3c689af37a7724966e64a02cf8c9d9a70ef
jl <jaeli_ottawa@outlook.com>
2025-03-16 13:20:29 -0400

    update admin sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,11 +1,58 @@
-import { Sidebar, SidebarContent, SidebarFooter } from "@/components/ui/sidebar"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton
+} from "@/components/ui/sidebar"
 import { Button } from "@/components/ui/button"
 import { useLogout } from "@/hooks"
 const AppSidebar = () => {
   const { logout } = useLogout()
+
+  const items = [
+    {
+      title: "Home",
+      url: "/admin",
+    },
+    {
+      title: "Products",
+      url: "/admin/products",
+    },
+    {
+      title: "Stocks",
+      url: "#",
+    },
+    {
+      title: "Orders",
+      url: "#",
+    }
+  ]
+
   return (
     <Sidebar>
-      <SidebarContent />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Management Dashboard</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
       <SidebarFooter>
         <Button onClick={() => logout()} >Logout</Button>
       </SidebarFooter>
