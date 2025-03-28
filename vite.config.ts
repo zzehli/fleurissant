25aa8d21248a2766792401525884d80a28216bcb
jl <jaeli_ottawa@outlook.com>
2025-03-28 19:23:40 -0400

    fix: minor change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
-  base: "/fleurissant",
+  base: "/fleurissant/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
