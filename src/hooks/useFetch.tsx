0c3e6ac078a295896c87ef287f4272e6132641ad
jl <jaeli_ottawa@outlook.com>
2025-04-07 15:11:31 -0400

    chore: spacing changes

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,42 +1,42 @@
 import { useEffect, useState } from "react";
-const useFetch= <T,> (url: string, admin: boolean = false) => {
-    const [data, setData] = useState<T>();
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
+const useFetch = <T,>(url: string, admin: boolean = false) => {
+  const [data, setData] = useState<T>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        setIsError(false);
-        setIsLoading(true);
-  
-        try {
-          const result = await fetch(url, {
-            headers: {
-              'Authorization': admin ? `Bearer ${localStorage.getItem('user')}` : ''
-            }
-          });
-          console.log("result", result)
-          if (!result.ok) {
-            console.log("error")
-            const errorMsg = await result.text()
-            throw new Error(`Request error! status: ${result.status}, message: ${errorMsg}`)
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await fetch(url, {
+          headers: {
+            'Authorization': admin ? `Bearer ${localStorage.getItem('user')}` : ''
           }
-          const data = await result.json()
-          console.log("data", data)
-
-          setData(data)
-        } catch (error) {
-          setIsError(true);
-          console.log(error)
+        });
+        console.log("result", result)
+        if (!result.ok) {
+          console.log("error")
+          const errorMsg = await result.text()
+          throw new Error(`Request error! status: ${result.status}, message: ${errorMsg}`)
         }
-  
-        setIsLoading(false);
-      };
-  
-      fetchData();
-    }, [url]);
-  
-    return { data, isLoading, isError };
-  };
+        const data = await result.json()
+        console.log("data", data)
 
-  export default useFetch;
+        setData(data)
+      } catch (error) {
+        setIsError(true);
+        console.log(error)
+      }
+
+      setIsLoading(false);
+    };
+    console.log('url fetch run', fetch)
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, isError };
+};
+
+export default useFetch;
--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,20 +1,16 @@
-import { Product } from '@/@types'
-import { Gallery, Navbar } from '@/components'
-import { useFetch } from '@/hooks'
-import { config } from '@/config'
+import { ScrollableGallery, Navbar } from '@/components'
 
 const Collection = () => {
-  const { data: products } = useFetch<Product[]>(`${config.urls.SERVER_URL}/products`)
-    
+
   return (
     <>
-        <Navbar />
-        <div className="container mx-auto px-4 md:px-6">
-            <h2 className="text-3xl font-bold mt-10 text-center tracking-tighter sm:text-3xl md:text-3xl">
-                Collection
-            </h2>
-            <Gallery products={products} />
-        </div>
+      <Navbar />
+      <div className="container mx-auto px-4 md:px-6">
+        <h2 className="text-3xl font-bold mt-10 text-center tracking-tighter sm:text-3xl md:text-3xl">
+          Collection
+        </h2>
+        <ScrollableGallery />
+      </div>
     </>
 
   )
