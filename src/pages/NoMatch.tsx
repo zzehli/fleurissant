86214e756c14c1d9d20f4d2a305555fead9023f0
jl <jaeli_ottawa@outlook.com>
2025-04-23 11:48:06 -0400

    feat: no match page

---
+++ src/pages/NoMatch.tsx
@@ -0,0 +1,11 @@
+import icon from '@/assets/branch-leaves.png'
+const NoMatch = () => {
+    return (
+        <div>
+            <img src={icon} alt="brand image" className="w-8 inline" /> <span>404: Page not found</span>
+        </div>
+
+    )
+}
+
+export default NoMatch
